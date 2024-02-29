9c9d6ee52803db0d7953a14e8ad62264919ce2a0
thCoders2 <137647343+thCoders2@users.noreply.github.com>
2024-02-29 08:09:40 -0300

    Criando árvore de talentos

---
+++ skills.py
@@ -0,0 +1,12 @@
+class skillPixo:
+    guiaNiveis = {
+        'basico':['rabiscar'],
+        'desenrolo':['degrade', 'linesWave', 'grafite,]
+    }
+    pass
+
+class skillAlquimia:
+    guiaNiveis = {
+        'basico':['trocarCap', 'refilCanetao'],
+        'pensante1':['passinhoCadaVez', 'sacolaToBackpack'],
+        'desenrolo': ['customizarSpray']
